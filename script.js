ed9e63ee2320495bcdaa6f2832a07963f3d6d378
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-01-14 12:51:11 -0600

    Start with pseudo-code

---
+++ script.js
@@ -0,0 +1,14 @@
+// take an array as an argument
+
+// if the array length is 1 return it as sorted
+
+// if the array length is greater than 1 split it into 2 halves with any odd numbers splitting the same way every time
+
+// call the function recursively on those two array halves
+
+// compare the two returned arrays and sort them by comparing the first number in each array;
+// the smallest number gets added to a new array. Keep doing this until both old arrays are
+// empty. If one array is empty then just push all remaining items from the non-empty array 
+// to the end of the new array
+
+// when all the comparing is complete return the new array
